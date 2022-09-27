46f98d89a7526e4fb6f0e88de7f68ab143aca11c
EngenheiraFreitas <alessandra.saldanha2009@gmail.com>
2022-09-27 09:11:50 -0300

    AppNarraLuc

---
+++ Screens/Search.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SearchScreen extends Component {
+
+    render(){
+        return (
+            <View style={styles.container}>
+              <Text style={styles.text}>Tela de Pesquisa</Text>
+            </View>
+        );
+    }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5653D4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 30
+  }
+});
